1f3e7fddb9e2e445e1fdc808464de6b6ec14ed1d
sepro <sebastian.proost@gmail.com>
2019-04-05 10:56:59 +0200

    used black to format code

--- app.py
+++ app.py
@@ -16,36 +16,29 @@ address_schema = {
         "FirstName": {"type": "string"},
         "LastName": {"type": "string"},
         "StreetName": {"type": "string"},
-        "StreetNumber": {
-            "oneOf": [
-                {"type": "string"},
-                {"type": "number"}
-            ]},
-        "BoxNumber": {
-            "oneOf": [
-                {"type": "string"},
-                {"type": "number"}
-            ]},
-        "PostalCode": {
-            "oneOf": [
-                {"type": "string"},
-                {"type": "number"}
-            ]},
+        "StreetNumber": {"oneOf": [{"type": "string"}, {"type": "number"}]},
+        "BoxNumber": {"oneOf": [{"type": "string"}, {"type": "number"}]},
+        "PostalCode": {"oneOf": [{"type": "string"}, {"type": "number"}]},
         "MunicipalityName": {"type": "string"},
         "CountryName": {"type": "string"},
     },
-    "required": [
-        "StreetName",
-        "StreetNumber",
-        "PostalCode",
-        "MunicipalityName"
-    ]
+    "required": ["StreetName", "StreetNumber", "PostalCode", "MunicipalityName"],
 }
 
 
-def validate_on_bpost(StreetName="", StreetNumber="", PostalCode="", MunicipalityName="", BoxNumber="", CountryName="",
-                      Title="", FirstName="", LastName="", timeout=3000,
-                      **kwargs):
+def validate_on_bpost(
+    StreetName="",
+    StreetNumber="",
+    PostalCode="",
+    MunicipalityName="",
+    BoxNumber="",
+    CountryName="",
+    Title="",
+    FirstName="",
+    LastName="",
+    timeout=3000,
+    **kwargs
+):
     """
     Takes parts from a standard (Belgian) address, validates using the BPost Webservice and returns the full response
 
@@ -78,7 +71,7 @@ def validate_on_bpost(StreetName="", StreetNumber="", PostalCode="", Municipalit
                                 "StructuredAddresseeIndividualIdentification": {
                                     "AddresseeFormOfAddress": Title,
                                     "AddresseeGivenName": FirstName,
-                                    "AddresseeSurname": LastName
+                                    "AddresseeSurname": LastName,
                                 }
                             }
                         },
@@ -87,19 +80,19 @@ def validate_on_bpost(StreetName="", StreetNumber="", PostalCode="", Municipalit
                                 "StructuredDeliveryPointLocation": {
                                     "StreetName": StreetName,
                                     "StreetNumber": str(StreetNumber),
-                                    "BoxNumber": str(BoxNumber)
+                                    "BoxNumber": str(BoxNumber),
                                 }
                             },
                             "PostalCodeMunicipality": {
                                 "StructuredPostalCodeMunicipality": {
                                     "PostalCode": str(PostalCode),
-                                    "MunicipalityName": MunicipalityName
+                                    "MunicipalityName": MunicipalityName,
                                 }
                             },
-                            "CountryName": CountryName
+                            "CountryName": CountryName,
                         },
                         "DispatchingCountryISOCode": "BE",
-                        "DeliveringCountryISOCode": "BE"
+                        "DeliveringCountryISOCode": "BE",
                     }
                 ]
             },
@@ -109,15 +102,13 @@ def validate_on_bpost(StreetName="", StreetNumber="", PostalCode="", Municipalit
                 "IncludeSubmittedAddress": True,
                 "IncludeDefaultGeoLocation": True,
                 "IncludeListOfBoxes": False,
-                "IncludeNumberOfBoxes": False
+                "IncludeNumberOfBoxes": False,
             },
-            "CallerIdentification": {
-                "CallerName": "VIB KULeuven"
-            }
+            "CallerIdentification": {"CallerName": "VIB KULeuven"},
         }
     }
 
-    r = requests.post(url, data=json.dumps(payload), timeout=timeout/1000)
+    r = requests.post(url, data=json.dumps(payload), timeout=timeout / 1000)
 
     return r.json()
 
@@ -132,64 +123,66 @@ def parse_bpost_validation(payload, response):
     """
     errors = 0
     warnings = 0
-    output = {
-        'status': 'validated',
-        'fields': {},
-        'formatted': {}
-    }
+    output = {"status": "validated", "fields": {}, "formatted": {}}
 
     response_errors = list(find("Error", response))
 
     # If 0 < length of list, there are errors which need to be handled
     if 0 < len(response_errors):
         for response_error in response_errors[0]:
-            if isinstance(response_error, dict) and 'ErrorSeverity' in response_error.keys():
-                if response_error['ErrorSeverity'] == 'warning':
+            if (
+                isinstance(response_error, dict)
+                and "ErrorSeverity" in response_error.keys()
+            ):
+                if response_error["ErrorSeverity"] == "warning":
                     warnings += 1
-                elif response_error['ErrorSeverity'] == 'error':
+                elif response_error["ErrorSeverity"] == "error":
                     errors += 1
 
-            if isinstance(response_error, dict) and 'ComponentRef' in response_error.keys():
-                if response_error['ComponentRef'] != '':
-                    component = list(find(response_error['ComponentRef'], response))
+            if (
+                isinstance(response_error, dict)
+                and "ComponentRef" in response_error.keys()
+            ):
+                if response_error["ComponentRef"] != "":
+                    component = list(find(response_error["ComponentRef"], response))
 
-                    output['fields'][response_error['ComponentRef']] = {
-                        'valid': False,
-                        'suggestion': string.capwords(component[0]) if 0 < len(component) else ""
+                    output["fields"][response_error["ComponentRef"]] = {
+                        "valid": False,
+                        "suggestion": string.capwords(component[0])
+                        if 0 < len(component)
+                        else "",
                     }
 
     # Add fields without a specific error to output
     # Some fields that are considered valid, are still different from the suggestion (!) this code will
     # include these as well.
     for field in payload.keys():
-        if field not in output['fields'].keys():
+        if field not in output["fields"].keys():
 
-            validated_field = ''.join(list(find(field, response)))
+            validated_field = "".join(list(find(field, response)))
 
-            if 0 < len(validated_field) and str(payload[field]).strip().lower() != str(validated_field).strip().lower():
-                output['fields'][field] = {
-                    'valid': False,
-                    'suggestion': string.capwords(validated_field)
+            if (
+                0 < len(validated_field)
+                and str(payload[field]).strip().lower()
+                != str(validated_field).strip().lower()
+            ):
+                output["fields"][field] = {
+                    "valid": False,
+                    "suggestion": string.capwords(validated_field),
                 }
 
                 warnings += 1
             else:
-                output['fields'][field] = {
-                    'valid': True,
-                    'suggestion': payload[field]
-                }
+                output["fields"][field] = {"valid": True, "suggestion": payload[field]}
 
     if 0 < errors:
-        output['result'] = 'error'
+        output["result"] = "error"
     elif 0 < warnings:
-        output['result'] = 'warning'
+        output["result"] = "warning"
     else:
-        output['result'] = 'valid'
+        output["result"] = "valid"
 
-    output['counts'] = {
-        'errors': errors,
-        'warnings': warnings
-    }
+    output["counts"] = {"errors": errors, "warnings": warnings}
 
     # Add formatted address to output
     formatted_submitted_address = list(find("FormattedSubmittedAddress", response))
@@ -197,22 +190,22 @@ def parse_bpost_validation(payload, response):
 
     if 0 < len(formatted_submitted_address):
         if "Line" in formatted_submitted_address[0].keys():
-            output['formatted']["submitted"] = formatted_submitted_address[0]["Line"]
+            output["formatted"]["submitted"] = formatted_submitted_address[0]["Line"]
         else:
-            output['formatted']["submitted"] = []
+            output["formatted"]["submitted"] = []
 
     if 0 < len(formatted_validated_address):
         if "Line" in formatted_validated_address[0].keys():
-            output['formatted']["validated"] = formatted_validated_address[0]["Line"]
+            output["formatted"]["validated"] = formatted_validated_address[0]["Line"]
         else:
-            output['formatted']["validated"] = []
+            output["formatted"]["validated"] = []
 
     # output['full'] = response
 
     return output
 
 
-@app.route('/validate', methods=['POST'])
+@app.route("/validate", methods=["POST"])
 def validate_address():
     """
     API endpoint that takes a JSON object with an address, checks if key components (StreetName, ...) are included,
@@ -227,32 +220,26 @@ def validate_address():
     except ValidationError as e:
         # If the input doesn't match the expectation, capture the error and return an object with details and
         # the appropriate http error code (417, Expectation Error)
-        exception = {
-            "status": "error",
-            "message": str(e)
-        }
-        return Response(json.dumps(exception), mimetype='application/json', status=417)
+        exception = {"status": "error", "message": str(e)}
+        return Response(json.dumps(exception), mimetype="application/json", status=417)
 
     try:
         bpost_validation = validate_on_bpost(**data)
     except Exception as e:
         # If this step fails the bpost service is unavailable, capture error and return an object with details and
         # the appropriate http error code (503, Service Unavailable)
-        exception = {
-            "status": "error",
-            "message": str(e)
-        }
+        exception = {"status": "error", "message": str(e)}
 
-        return Response(json.dumps(exception), mimetype='application/json', status=503)
+        return Response(json.dumps(exception), mimetype="application/json", status=503)
 
     output = parse_bpost_validation(data, bpost_validation)
 
-    return Response(json.dumps(output), mimetype='application/json')
+    return Response(json.dumps(output), mimetype="application/json")
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
